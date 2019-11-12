31f69d317b7930a56b04c6214a3d076e029495e8
Alfonso <alfonso.garcia2324@gmail.com>
2019-11-12 09:55:38 -0800

    Added 2nd attempt to remove duplicate elements in array

--- Arrays/Remove_Duplicates.js
+++ Arrays/Remove_Duplicates.js
@@ -18,14 +18,53 @@
     
     Return the length of the arr nums
 */
-var removeDuplicates = function(nums) {
-    const cache = {};
 
+// First attempt
+
+// var removeDuplicates = function(nums) {
+//     const cache = {};
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] in cache) {
+//             nums.splice(i, i);
+//         } else {
+//             cache[nums[i]] = true;
+//         }
+//     }
+
+//     return nums.length;
+// };
+
+// ======================================================
+
+// Second attempt
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*
+
+    [0, 0, 0, 1]
+
+    We're trying to remove any duplicate numbers in the array.
+    
+    Create a cache (dynamic approach)
+    
+    Loop through all the numbers
+        If the current number is not in the cache, add it.
+        else if the current number is in the cache already, delete it from the array
+        else ...
+    
+    Return the length of the arr nums
+*/
+var removeDuplicates = function(nums) {
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] in cache) {
-            nums.splice(i, i);
-        } else {
-            cache[nums[i]] = true;
+        if (nums[i + 1] === nums[i]) {
+            nums.splice(i + 1, 1);
+        } else if (nums[i - 1] === nums[i]) {
+            nums.splice(i - 1, 1);
         }
     }
 
