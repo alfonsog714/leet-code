85988cab96e5518d78c471db8c9bc3003ae41519
Alfonso <alfonso.garcia2324@gmail.com>
2020-01-25 15:25:03 -0800

    added example 2 for big o section of cracking the coding interview

---
+++ Non-leetcode/Cracking-the-Coding-Interview/Big-O/examples-2.js
@@ -0,0 +1,11 @@
+/*
+    What is the runtime of the code below?
+ */
+
+function printPairs(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            console.log(`${array[i]}, ${array[j]}`);
+        }
+    }
+}
