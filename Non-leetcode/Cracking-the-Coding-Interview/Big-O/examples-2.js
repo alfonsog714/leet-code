e25add7a16745b59c9e74ce6415c467a419f8a0b
Alfonso <alfonso.garcia2324@gmail.com>
2020-01-25 15:27:37 -0800

    added answer for example 2

--- Non-leetcode/Cracking-the-Coding-Interview/Big-O/examples-2.js
+++ Non-leetcode/Cracking-the-Coding-Interview/Big-O/examples-2.js
@@ -9,3 +9,13 @@ function printPairs(array) {
         }
     }
 }
+
+/* 
+    The runtime of the code above is O(n^2). The main giveaway for that is the nested for loop.
+
+    ====================
+    Answer from the book
+    ====================
+    The inner for loop has O(N) iterations and it is called N times. Therefore, the runtime is O(N^2).
+    Another way we can see this is by inspecting what the "meaning" of the code is. It is printing all pairs (two-element sequences). There are O(N^2) pairs; therefore, the runtime is O(N^2).
+*/
