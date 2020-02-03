8ebf21bfef73480fa6c85fba221f31adfa2e7a36
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-03 10:06:17 -0800

    finished LC20

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,25 @@
-const isValid = s => {};
+const isValid = s => {
+    let stack = [];
+    let pairs = { '(': ')', '{': '}', '[': ']' };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (pairs[char]) {
+            // I first did this with "if(char in pairs)", but apparently that's much slower than this method
+            stack.push(char);
+        } else {
+            const topOfStack = stack[stack.length - 1];
+            char === pairs[topOfStack] ? stack.pop() : stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
+
+/* 
+    Technique: Make use to a stack data structure
+    Time complexity: O(n)
+*/
 
 module.exports = isValid;
