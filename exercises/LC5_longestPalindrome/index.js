f17f958e9764e51d0d383cdc09c4f8da7479a474
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-03 09:54:25 -0800

    added solution to LC5

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,34 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    let start = 0;
+    let maxLength = 1;
+
+    function expandAroundMiddle(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            const currentPalLength = right - left + 1;
+            if (currentPalLength > maxLength) {
+                maxLength = currentPalLength;
+                start = left;
+            }
+            left -= 1;
+            right += 1;
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        expandAroundMiddle(i, i + 1);
+        expandAroundMiddle(i - 1, i + 1);
+    }
+
+    return s.slice(start, start + maxLength);
+}
+
+/* 
+    Emphasize the importance of this question, study this.
+    
+    Technique: Expand around the center
+*/
 
 module.exports = longestPalindrome;
