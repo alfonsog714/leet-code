ed74d581b3b4545ef362812662877fd8de11c34a
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-03 09:31:05 -0800

    finished LC3

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,41 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+// function lengthOfLongestSubstring(s) {
+//     let stringCount = 1;
+//     for (let i = 0; i < s.length; i++) {
+//         let currentLetter = s[i];
 
+//         for (let j = i + 1; j < s.length; j++) {
+//             if (s[j] === currentLetter) return stringCount;
+
+//             stringCount += 1;
+//         }
+//     }
+// }
+
+// Sliding Window approach
+function lengthOfLongestSubstring(s) {
+    const lettersMap = {};
+    let start = 0;
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const endChar = s[i];
+
+        if (lettersMap[endChar] >= start) {
+            start = lettersMap[endChar] + 1;
+        }
+
+        lettersMap[endChar] = i;
+        maxLength = Math.max(maxLength, i - start + 1);
+    }
+
+    return maxLength;
+}
+
+/* 
+    - The window is created with the starting index, start, and the ending index, which is the i in the for-loop.
+    - It's the sliding window approach because we conditionally move the beginning of the window as we iterate through the string.
+*/
 module.exports = lengthOfLongestSubstring;
