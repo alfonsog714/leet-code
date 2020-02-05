2e282fd8243426e98d58252d518f04ea126e419a
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-05 08:34:19 -0800

    working on LC49

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,22 @@
+/* 
+=======
+Problem
+=======
+- Group anagrams of a word into an array
+
+============
+Sub-problems
+============
+- remove words from the array so you don't duplicate them
+- find a word to compare the others to to see if it is an anagram
+
+==========
+Pseudocode
+==========
+
+
+*/
+
 function groupAnagrams(strs) {}
 
 module.exports = groupAnagrams;
