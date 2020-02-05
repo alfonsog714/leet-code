405b46c0e4ea56de68e750000029772ee2790693
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-05 09:24:51 -0800

    finished LC242

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,34 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+    const letters1 = {};
+    const letters2 = {};
+
+    if (s.length !== t.length) return false;
+
+    for (let char of s) {
+        if (!letters1[char]) {
+            letters1[char] = 1;
+        } else {
+            letters1[char] += 1;
+        }
+    }
+
+    for (let char of t) {
+        if (!letters2[char]) {
+            letters2[char] = 1;
+        } else {
+            letters2[char] += 1;
+        }
+    }
+
+    for (let char of s) {
+        if (!letters2[char]) {
+            return false;
+        } else if (letters1[char] !== letters2[char]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 module.exports = isAnagram;
