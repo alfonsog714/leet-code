208762c7bd1f866841a0768ca6c361e5e14506fc
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-06 09:30:45 -0800

    completed LC55

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,30 +1,54 @@
+// First solution
+// ===========================
+// function isAnagram(s, t) {
+//     const letters1 = {};
+//     const letters2 = {};
+
+//     if (s.length !== t.length) return false;
+
+//     for (let char of s) {
+//         if (!letters1[char]) {
+//             letters1[char] = 1;
+//         } else {
+//             letters1[char] += 1;
+//         }
+//     }
+
+//     for (let char of t) {
+//         if (!letters2[char]) {
+//             letters2[char] = 1;
+//         } else {
+//             letters2[char] += 1;
+//         }
+//     }
+
+//     for (let char of s) {
+//         if (!letters2[char]) {
+//             return false;
+//         } else if (letters1[char] !== letters2[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
 function isAnagram(s, t) {
-    const letters1 = {};
-    const letters2 = {};
+    const sCharCounts = {};
 
-    if (s.length !== t.length) return false;
-
-    for (let char of s) {
-        if (!letters1[char]) {
-            letters1[char] = 1;
-        } else {
-            letters1[char] += 1;
-        }
+    // Fill sCharCounts
+    for (let i = 0; i < s.length; i++) {
+        const sChar = s[i];
+        sCharCounts[sChar] = sCharCounts[sChar] + 1 || 1;
     }
 
-    for (let char of t) {
-        if (!letters2[char]) {
-            letters2[char] = 1;
-        } else {
-            letters2[char] += 1;
-        }
-    }
+    for (let i = 0; i < t.length; i++) {
+        const tChar = t[i];
 
-    for (let char of s) {
-        if (!letters2[char]) {
-            return false;
-        } else if (letters1[char] !== letters2[char]) {
-            return false;
+        if (!sCharCounts[tChar]) return false;
+        // 0 is a falsy value in JavaScript, so it covers the case when the key's value is 0 as well
+        else {
+            sCharCounts[tChar]--;
         }
     }
 
--- exercises/LC55_canJump/index.js
+++ exercises/LC55_canJump/index.js
@@ -1,3 +1,17 @@
-function canJump(nums) {}
+function canJump(nums) {
+    let dpPositions = new Array(nums.length).fill(false); // creates another array the size of nums with false values
+    dpPositions[0] = true; // sets the first value to true since you start off at that position
+
+    for (let j = 1; j < nums.length; j++) {
+        for (let i = 0; i < j; i++) {
+            if (dpPositions[i] && i + nums[i] >= j) {
+                dpPositions[j] = true;
+                break;
+            }
+        }
+    }
+
+    return dpPositions[dpPositions.length - 1];
+}
 
 module.exports = canJump;
