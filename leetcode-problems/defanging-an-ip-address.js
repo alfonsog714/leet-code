cae9ca06729a06d8ef39e0f585641c15f44c9d62
Alfonso <alfonso.garcia2324@gmail.com>
2020-02-11 13:57:59 -0800

    added solution to defanging an ip address

---
+++ leetcode-problems/defanging-an-ip-address.js
@@ -0,0 +1,34 @@
+/* 
+=======
+Problem
+=======
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+=========
+Example 1
+=========
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+
+=========
+Example 2
+=========
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+*/
+
+var defangIPaddr = function(address) {
+    let newAddress = address.split('');
+
+    for (let i = 0; i < newAddress.length; i++) {
+        if (newAddress[i] === '.') {
+            newAddress[i] = '[.]';
+        }
+    }
+
+    newAddress = newAddress.join('');
+    return newAddress;
+};
